72469c858e9955c138b0d6f4e5522f5b6d2bf614
javaria-gul <javariagul73@gmail.com>
2024-11-23 19:55:35 +0500

    Role Selection page created successfully

--- TRENDZZ/Form1.Designer.cs
+++ TRENDZZ/Form1.Designer.cs
@@ -29,12 +29,46 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblTrendzz = new System.Windows.Forms.Label();
+            this.timerFade = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblTrendzz
+            // 
+            this.lblTrendzz.BackColor = System.Drawing.Color.Black;
+            this.lblTrendzz.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTrendzz.Font = new System.Drawing.Font("Arial Black", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTrendzz.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblTrendzz.Location = new System.Drawing.Point(0, 0);
+            this.lblTrendzz.Name = "lblTrendzz";
+            this.lblTrendzz.Size = new System.Drawing.Size(978, 744);
+            this.lblTrendzz.TabIndex = 0;
+            this.lblTrendzz.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timerFade
+            // 
+            this.timerFade.Interval = 6000;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(978, 744);
+            this.Controls.Add(this.lblTrendzz);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTrendzz;
+        private System.Windows.Forms.Timer timerFade;
     }
 }
 
--- TRENDZZ/Form1.cs
+++ TRENDZZ/Form1.cs
@@ -1,81 +1,84 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-
 
 namespace TRENDZZ
 {
     public partial class Form1 : Form
     {
-        private Timer fadeTimer; // Timer for fading effect
-        private int fadeOpacity = 255; // Initial opacity
+        private Timer fadeTimer;
+        private int fadeOpacity = 255;
 
         public Form1()
         {
             InitializeComponent();
-            InitializeFadeTimer();
-            lblTrendzz.Paint += LblTrendzz_Paint; // Attach Paint event
+            InitializeFadeTimer(); // Initialize the fade timer
+            lblTrendzz.Paint += LblTrendzz_Paint; // Attach Paint event to lblTrendzz
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            // Clear any default label text and set transparent background
+            // Set up lblTrendzz for the initial display
             lblTrendzz.Text = "";
-            lblTrendzz.BackColor = Color.Transparent;
+            lblTrendzz.BackColor = Color.Black;
         }
 
         private void InitializeFadeTimer()
         {
+            // Timer for handling the fade effect
             fadeTimer = new Timer
             {
-                Interval = 200 // 100ms interval
+                Interval = 150 // Adjust for smooth fade effect
             };
             fadeTimer.Tick += FadeTimer_Tick;
-            fadeTimer.Start();
+            fadeTimer.Start(); // Start fading
         }
 
         private void FadeTimer_Tick(object sender, EventArgs e)
         {
-            fadeOpacity -= 15; // Gradually decrease opacity
+            // Gradually reduce opacity
+            fadeOpacity -= 15;
             if (fadeOpacity <= 0)
             {
                 fadeOpacity = 0;
-                fadeTimer.Stop(); // Stop the timer
-                lblTrendzz.Visible = false; // Hide the label
+                fadeTimer.Stop();
+                lblTrendzz.Visible = false; // Hide lblTrendzz after fade
+
+                // Transition to RoleSelectionForm
+                RoleSelectionForm roleSelection = new RoleSelectionForm();
+                roleSelection.Show();
+                this.Hide(); // Hide current form
             }
-            lblTrendzz.Invalidate(); // Force redraw
+            lblTrendzz.Invalidate(); // Redraw lblTrendzz with updated opacity
         }
 
         private void LblTrendzz_Paint(object sender, PaintEventArgs e)
         {
-            string text = "TRENDZZ";
-            Font font = new Font("Arial", 36, FontStyle.Bold);
+            // Custom rendering for lblTrendzz
+            e.Graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
 
-            // Brush array for colors
+            string text = "TRENDZZ";
+            Font font = new Font("Bernard MT", 30, FontStyle.Bold);
+
+            // Rainbow colors
             Brush[] brushes = new Brush[]
             {
-        Brushes.Red, Brushes.Orange, Brushes.Yellow, Brushes.Green,
-        Brushes.Blue, Brushes.Indigo, Brushes.Violet
+                Brushes.Red, Brushes.Orange, Brushes.Yellow, Brushes.Green,
+                Brushes.Blue, Brushes.Indigo, Brushes.Violet
             };
 
-            // Measure the total width of the text
+            // Calculate total text width for centering
             float totalWidth = 0;
             for (int i = 0; i < text.Length; i++)
             {
                 totalWidth += e.Graphics.MeasureString(text[i].ToString(), font).Width;
             }
 
-            // Calculate starting positions to center the text
-            float startX = (lblTrendzz.Width - totalWidth) / 2; // Center horizontally
-            float startY = (lblTrendzz.Height - font.Height) / 2; // Center vertically
+            // Calculate starting position for centered text
+            float startX = (lblTrendzz.Width - totalWidth) / 2;
+            float startY = (lblTrendzz.Height - font.Height) / 2;
 
-            // Draw each character in its respective color
+            // Draw each character with its corresponding color
             for (int i = 0; i < text.Length; i++)
             {
                 Color fadedColor = Color.FromArgb(fadeOpacity, ((SolidBrush)brushes[i]).Color);
@@ -83,7 +86,7 @@ namespace TRENDZZ
                 {
                     e.Graphics.DrawString(text[i].ToString(), font, fadedBrush, startX, startY);
                 }
-                startX += e.Graphics.MeasureString(text[i].ToString(), font).Width; // Increment X for the next character
+                startX += e.Graphics.MeasureString(text[i].ToString(), font).Width;
             }
         }
     }
